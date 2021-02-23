2851f26e92957c659ae87b512d6e2abae3de4511
triptisahu08 <tsahu1108@gmail.com>
2021-02-23 10:47:12 +0530

    New test file for sign_in screen

---
+++ test_driver/test_screens/sin_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void sin()
+{
+
+   final btnSignUp = find.byValueKey('btnSignUp');
+  group('E-commerce:', () {
+    final btnSplash = find.byValueKey('btnSplash');
+    FlutterDriver driver;
+
+      setUpAll(() async {
+        driver = await FlutterDriver.connect();
+      });
+
+      tearDownAll(() async {
+        if (driver != null) {
+          driver.close();
+        }
+      });
+      group('Sign in screen-',(){
+      test('Sign up button',()async{
+        await Future.delayed(Duration(milliseconds: 1000));
+        await driver.tap(btnSignUp);
+      });
+    });//Sign in screen 
+  });
+}
